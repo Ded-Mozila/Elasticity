565ade30f42ca27d87fc1c0399863345d0ba9bb9
Ded-Mozila <zyryanov.aleksander@outlook.com>
2014-11-22 13:40:47 +1000

    Проверка k узлов на включение в облать(матрицу)

--- build_integral_plane.cpp
+++ build_integral_plane.cpp
@@ -17,43 +17,59 @@ integral::integral(double h, int N)
 	Fi[5] = make_pair(0.0,1/h); 
 	Fi[6] = make_pair(-1/h,1/h);
 }
-
-double integral::BuildAreaElm_K(int  k, int xi, int xj int row, double column)
+bool integral::NodeBelongsRegions(int row, int column, int h, int l, int k)
 {
-	double area = 0;
-	switch(k)
+	
+	return false;
+}
+map<int,bool> integral::BuildAreaElm_K( int l, int h int row, double column)
+{
+	map<int,bool> K;
+	for(int i = 1 ; i< 7 ; ++i)
+	switch(i)
 	{
-		case 0:
-		{
-			//проити по кругу и определить какие из тереугольников пренадлежат
-			if(column+1 > 0 || column+1 < N)
-			
-			break;
-		}
 		case 1:
 		{
+			if(( 0 <= row && row < N ) && ( 0 <= column && column < N-1 ))
+				K[1] = true;
+			else K[1] = false;	
 			break;
 		}
 		case 2:
 		{
+			if(( 1 < row && row < N ) && ( 0 <= column && column < N-1))
+				K[2] = true;
+			else K[2] = false;
 			break;
 		}
 		case 3:
 		{
+			if(( 1 < row && row < N ) && ( 0 <= column && column < N ))
+				K[3] = true;
+			else K[3] = false; 
 			break;
 		}
 		case 4:
 		{
+			if(( 0 <= row && row < N ) && ( 1 < column && column < N ))
+				K[4] = true;
+			else k[4] = false;	
 			break;
 		}
 		case 5:
 		{
+			if(( 0 <= row && row < N-1 ) && ( 1 < column && column < N))
+				K[5] = true;
+			else K[5] = false;
 			break;
 		}
 		case 6:
-		{
+		{	
+			if(( 0 <= row && row < N-1 ) && ( 0 < column && column < N))
+				k[6] = true;
+			else k[6] = false;
 			break;
 		}
 	}
-	return area;
+	return K;
 }
